64f547a4312b7e3f8b68ca9324cbfc49c0ffb272
GhostWalker562 <43276017+GhostWalker562@users.noreply.github.com>
2025-04-14 14:32:23 -0700

    Export TransactionParser default

---
+++ .changeset/real-snakes-warn.md
@@ -0,0 +1,6 @@
+---
+"@aptos-labs/js-pro": patch
+"@aptos-labs/react": patch
+---
+
+Export TransactionParser default
--- packages/js-pro/src/utils/transactions/index.ts
+++ packages/js-pro/src/utils/transactions/index.ts
@@ -1,4 +1,4 @@
-export * from "./TransactionParser.js";
+export { default as TransactionParser } from "./TransactionParser.js";
 export * from "./parsers/CoinEventParser.js";
 export * from "./parsers/FungibleAssetEventParser.js";
 export * from "./parsers/CoinStoreWritesetParser.js";
